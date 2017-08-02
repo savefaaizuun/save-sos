ed8b9de0d26d89d5120e586d4672a695bcb7cba9
Asep Cahyudin <asep.cahyudin@divusi.com>
2017-08-02 14:54:28 +0700

    update

--- resources/views/layouts/includes/sidebar.blade.php
+++ resources/views/layouts/includes/sidebar.blade.php
@@ -1,92 +1,107 @@
 <div class="sidebar-collapse">
-                    <ul class="nav" id="side-menu">
-                        <li class="nav-header">
-                            <div class="dropdown profile-element"> <span>
-                                <img alt="image" class="img-circle" src="img/profile_small.jpg" />
-                            </span>
-                            <a data-toggle="dropdown" class="dropdown-toggle" href="#">
-                                <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">David Williams</strong>
-                                    </span> <span class="text-muted text-xs block">Art Director <b class="caret"></b></span> </span> </a>
-                                    <ul class="dropdown-menu animated fadeInRight m-t-xs">
-                                        <li><a href="profile.html">Profile</a></li>
-                                        <li><a href="contacts.html">Contacts</a></li>
-                                        <li><a href="mailbox.html">Mailbox</a></li>
-                                        <li class="divider"></li>
-                                        <li><a href="login.html">Logout</a></li>
-                                    </ul>
-                                </div>
-                                <div class="logo-element">
-                                    IN+
-                                </div>
-                            </li>
-                            <li>
-                                <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboards</span> <span class="fa arrow"></span></a>
-                                <ul class="nav nav-second-level">
-                                    <li><a href="index.html">Dashboard v.1</a></li>
-                                    <li><a href="dashboard_2.html">Dashboard v.2</a></li>
-                                    <li><a href="dashboard_3.html">Dashboard v.3</a></li>
-                                    <li><a href="dashboard_4_1.html">Dashboard v.4</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                               <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Profil</span><span class="fa arrow"></span></a>
-                               <ul class="nav nav-second-level">
-                                   <li><a href="{{ url('admin/prodi') }}">Sejarah Singkat</a></li>
-                                   <li><a href="{{ url('admin/mapel') }}">Visi dan Misi</a></li>
-                                   <li><a href="{{ url('admin/ruang') }}">Struktur Organisasi</a></li>
-                                   <li><a href="{{ url('admin/rombel') }}">Data Pokok Sekolah</a></li>
-                               </ul>
-                           </li>
-                            <li>
-                                <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Master Data</span><span class="fa arrow"></span></a>
-                                <ul class="nav nav-second-level">
-                                    <li><a href="{{ url('admin/prodi') }}">Program Studi</a></li>
-                                    <li><a href="{{ url('admin/mapel') }}">Mata Pelajaran</a></li>
-                                    <li><a href="{{ url('admin/ruang') }}">Ruangan</a></li>
-                                    <li><a href="{{ url('admin/rombel') }}">Rombongan Belajar</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="mailbox.html"><i class="fa fa-envelope"></i> <span class="nav-label">Konfigurasi Akademik </span><span class="fa arrow"></span></a>
-                                <ul class="nav nav-second-level">
-                                    <li><a href="{{ url('admin/tahun_akademik') }}">Tahun Akademik</a></li>
-                                    <li><a href="{{ url('admin/kurikulum') }}">Kurikulum</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Warga Sekolah</span><span class="fa arrow"></span></a>
-                                <ul class="nav nav-second-level">
-                                    <li><a href="{{ url('admin/guru') }}">Data Guru</a></li>
-                                    <li><a href="{{ url('admin/staff') }}">Data Staff</a></li>
-                                    <li><a href="{{ url('admin/siswa') }}">Data Siswa</a></li>
-                                    <li><a href="{{ url('admin/alumni') }}">Data Alumni</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                                <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Data Absensi</span><span class="fa arrow"></span></a>
-                                <ul class="nav nav-second-level">
-                                    <li><a href="{{ url('admin/guru') }}">Upload Absensi Siswa</a></li>
-                                    <li><a href="{{ url('admin/staff') }}">Upload Absensi Guru</a></li>
-                                    <li><a href="{{ url('admin/siswa') }}">Upload Absensi Staff</a></li>
-                                </ul>
-                            </li>
-                            <li>
-                               <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Data Pengguna</span><span class="fa arrow"></span></a>
-                               <ul class="nav nav-second-level">
-                                   <li><a href="{{ url('admin/guru') }}">Siswa</a></li>
-                                   <li><a href="{{ url('admin/staff') }}">Guru</a></li>
-                                   <li><a href="{{ url('admin/siswa') }}">Kepala Sekolah</a></li>
-                                   <li><a href="{{ url('admin/alumni') }}">Administrator</a></li>
-                               </ul>
-                           </li>
-                           <li>
-                              <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Laporan Nilai Siswa</span><span class="fa arrow"></span></a>
-                              <ul class="nav nav-second-level">
-                                  <li><a href="{{ url('admin/guru') }}">Data Nilai UTS</a></li>
-                                  <li><a href="{{ url('admin/staff') }}">Data Nilai UAS</a></li>
-                                  <li><a href="{{ url('admin/siswa') }}">Data Capaian Belajar</a></li>
-                                  <li><a href="{{ url('admin/alumni') }}">Cetak Raport</a></li>
-                              </ul>
-                          </li>
-                                </ul>
-                            </div>
+  <ul class="nav" id="side-menu">
+    <li class="nav-header">
+      <div class="dropdown profile-element"> <span>
+        <img alt="image" class="img-circle" src="img/profile_small.jpg" />
+      </span>
+      <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+        <span class="clear"> <span class="block m-t-xs"> <strong class="font-bold">David Williams</strong>
+          </span> <span class="text-muted text-xs block">Art Director <b class="caret"></b></span> </span> </a>
+          <ul class="dropdown-menu animated fadeInRight m-t-xs">
+            <li><a href="profile.html">Profile</a></li>
+            <li><a href="contacts.html">Contacts</a></li>
+            <li><a href="mailbox.html">Mailbox</a></li>
+            <li class="divider"></li>
+            <li><a href="login.html">Logout</a></li>
+          </ul>
+        </div>
+        <div class="logo-element">
+          IN+
+        </div>
+      </li>
+      <li>
+        <a href="index.html"><i class="fa fa-th-large"></i> <span class="nav-label">Dashboards</span> <span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li><a href="index.html">Dashboard v.1</a></li>
+          <li><a href="dashboard_2.html">Dashboard v.2</a></li>
+          <li><a href="dashboard_3.html">Dashboard v.3</a></li>
+          <li><a href="dashboard_4_1.html">Dashboard v.4</a></li>
+        </ul>
+      </li>
+      <!-- <li>
+        <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Master Data</span><span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li><a href="{{ url('admin/prodi') }}">Sejarah Singkat</a></li>
+          <li><a href="{{ url('admin/mapel') }}">Visi dan Misi</a></li>
+          <li><a href="{{ url('admin/ruang') }}">Struktur Organisasi</a></li>
+          <li><a href="{{ url('admin/rombel') }}">Data Pokok Sekolah</a></li>
+        </ul>
+      </li> -->
+      <li>
+        <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Master Data</span><span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li><a href="{{ url('admin/siswa') }}">Siswa</a></li>
+          <li><a href="{{ url('admin/guru') }}">Guru</a></li>
+          <li><a href="{{ url('admin/staff') }}">Staff</a></li>
+          <li><a href="{{ url('admin/prodi') }}">Program Studi</a></li>
+          <li><a href="{{ url('admin/mapel') }}">Mata Pelajaran</a></li>
+          <li><a href="{{ url('admin/ruang') }}">Ruangan</a></li>
+          <li><a href="{{ url('admin/rombel') }}">Rombongan Belajar</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="mailbox.html"><i class="fa fa-envelope"></i> <span class="nav-label">Konfigurasi Akademik </span><span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li><a href="{{ url('admin/tahun_akademik') }}">Tahun Akademik</a></li>
+          <li><a href="{{ url('admin/kurikulum') }}">Kurikulum</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Kegiatan Akademik</span><span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li><a href="{{ url('admin/guru') }}">Jadwal</a></li>
+          <li><a href="{{ url('admin/staff') }}">Absensi Siswa</a></li>
+          <li><a href="{{ url('admin/siswa') }}">Absensi Guru</a></li>
+          <li><a href="{{ url('admin/alumni') }}">Absensi Staff</a></li>
+          <li><a href="{{ url('admin/alumni') }}">Pengolahan Nilai</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Keuangan</span><span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li><a href="{{ url('admin/guru') }}">Input Pembayaran</a></li>
+          <li><a href="{{ url('admin/staff') }}">Jenis Pembayaran</a></li>
+          <li><a href="{{ url('admin/siswa') }}">Biaya Sekolah</a></li>
+          <li><a href="{{ url('admin/siswa') }}">Laporan Keuangan</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Data Pengguna</span><span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li><a href="{{ url('admin/guru') }}">Siswa</a></li>
+          <li><a href="{{ url('admin/staff') }}">Guru</a></li>
+          <li><a href="{{ url('admin/siswa') }}">Kepala Sekolah</a></li>
+          <li><a href="{{ url('admin/alumni') }}">Administrator</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">Laporan Nilai Siswa</span><span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li><a href="{{ url('admin/guru') }}">Data Nilai UTS</a></li>
+          <li><a href="{{ url('admin/staff') }}">Data Nilai UAS</a></li>
+          <li><a href="{{ url('admin/siswa') }}">Data Capaian Belajar</a></li>
+          <li><a href="{{ url('admin/alumni') }}">Cetak Raport</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#"><i class="fa fa-bar-chart-o"></i> <span class="nav-label">CMS</span><span class="fa arrow"></span></a>
+        <ul class="nav nav-second-level">
+          <li><a href="#">Menu Bar</a></li>
+          <li><a href="#">Konten</a></li>
+          <li><a href="#">Berita</a></li>
+          <li><a href="#">Side Bar</a></li>
+          <li><a href="#">Footer</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
